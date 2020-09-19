c8b124c4fcbc6aceb7dbafd3d2b7a59595a751de
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2020-09-19 14:22:35 -0500

    Toggle login view added

---
+++ public/javascripts/login.js
@@ -0,0 +1,20 @@
+const logForm = document.getElementById('login');
+const regForm = document.getElementById('register');
+const btnLog = document.getElementById('btn-log');
+const btnReg = document.getElementById('btn-reg');
+
+const register = () => {
+  logForm.style.left = '-400px';
+  regForm.style.left = '50px';
+  btnReg.classList.remove('btn-secondary');
+  btnReg.classList.add('btn-primary');
+  btnLog.classList.add('btn-secondary');
+};
+
+const login = () => {
+  logForm.style.left = '50px';
+  regForm.style.left = '450px';
+  btnLog.classList.remove('btn-secondary');
+  btnLog.classList.add('btn-primary');
+  btnReg.classList.add('btn-secondary');
+};
