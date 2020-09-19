04b7afab4c9a1362617bef1e321c076d526665a1
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2020-09-19 13:21:25 -0500

    remove node_modules folder

--- app.js
+++ app.js
@@ -4,29 +4,22 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 
 const login = require('./api/login');
-const users = require('./routes/users');
+// const users = require('./routes/users');
 
 const app = express();
-
-// view engine setup
-/*
- app.set('views', path.join(__dirname, 'views'));
- app.set('view engine', 'pug');
-*/
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', login);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
