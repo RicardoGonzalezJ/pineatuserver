0bb55360ba417d543ef024a9ebbbdf519c06ea18
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2020-10-27 22:37:17 -0500

    create a pool connection with database

--- config/dbcon/dbcon.js
+++ config/dbcon/dbcon.js
@@ -7,7 +7,8 @@ const pool = new Pool(datasource);
 if (!pool) {
   console.log('connection error...');
 } else {
-  console.log('connected to postgresql to database:', pool.options.database);
+  console.log('connected to postgresql to database:',
+    pool.options.datasource.database);
 }
 
 module.exports = pool;
