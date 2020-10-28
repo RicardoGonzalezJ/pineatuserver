998a486f526056d6b5928528d5ef6a7efe69c7cf
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2020-10-27 23:41:40 -0500

    passport configuration added

---
+++ config/passport/serialize.js
@@ -0,0 +1,11 @@
+const passport = require('passport');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+};
