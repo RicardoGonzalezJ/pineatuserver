6cbceebb8d712c4673a1cbeb354d325da69463cc
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2020-10-27 23:57:38 -0500

    connection created to work with sessions

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const exsession = require('express-session');
+const ConPgSimple = require('connect-pg-simple')(exsession);
+
+const dbcon = require('./config/dbcon/dbcon');
 
 const login = require('./api/login');
 const user = require('./api/user');
@@ -17,6 +21,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(exsession({
+  store: new ConPgSimple({
+    pool: dbcon,
+    tableName: 'session',
+  }),
+  secret: process.env.PORTAL_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 3600000,
+    httpOnly: false,
+  },
+
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', login);
