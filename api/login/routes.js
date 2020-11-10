a6606eab2ebfebfeb21e198a4ca023e80338eb8f
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2020-11-09 22:29:22 -0500

    feat: routes.js Add signin and signup feature

--- api/login/routes.js
+++ api/login/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const test = require('./controller');
+const ctr = require('./controller');
 
-router.get('/', test.hello);
+router.get('/', ctr.renderLogin);
+
+router.post('/', ctr.signin);
+
+router.post('/', ctr.signup);
 
 module.exports = router;
