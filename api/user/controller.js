be9eed90f829d3ba9d2bbd0305430826bbe7df48
RicardoGonzalezJ <rgonzalezj1480@gmail.com>
2020-11-11 11:27:33 -0500

    Fix:(user/controller.js) Initialize key/value in json response.
    Change from res.status(201).json({ userList}) to
    res.status(201).json({ userLists: userList })
    
    BREAKING CHANGE: this fix error on user controller.js listOfUsers Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client.

--- api/user/controller.js
+++ api/user/controller.js
@@ -3,7 +3,7 @@ const model = require('./model');
 exports.listOfUsers = async (req, res) => {
   try {
     const userList = await model.selectForUser();
-    res.status(201).json({ userList });
+    res.status(201).json({ userLists: userList });
     return;
   } catch (error) {
     console.log('error on user controller.js listOfUsers', error);
